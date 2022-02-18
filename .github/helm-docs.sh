b86e433dd5851d236e7abb2a857dc6f230ce25c8
Gabriel Melillo <gabriel@melillo.me>
2022-02-18 22:01:47 +0100

    ci(docs): new version of the docs
    Signed-off-by: Gabriel Melillo <gabriel@melillo.me>

--- .github/helm-docs.sh
+++ .github/helm-docs.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 
-HELM_DOCS_VERSION="v1.4.0"
+HELM_DOCS_VERSION="v1.7.0"
 
 # install helm-docs
 curl --silent --show-error --fail --location --output /tmp/helm-docs.tar.gz https://github.com/norwoodj/helm-docs/releases/download/"${HELM_DOCS_VERSION}"/helm-docs_"${HELM_DOCS_VERSION}"_Linux_x86_64.tar.gz
 tar -xf /tmp/helm-docs.tar.gz helm-docs
 
 # validate docs
-./helm-docs --template-files=./charts/README.md.gotmpl
+./helm-docs --chart-search-root=charts --template-files=charts/README.md.gotmpl
 git diff --exit-code
